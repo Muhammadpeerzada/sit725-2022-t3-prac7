578e64addd7698ccbfbc56fbbd810b7fd167c61f
Muhammadpeerzada <109573696+Muhammadpeerzada@users.noreply.github.com>
2023-01-13 15:33:08 +1100

    Read function Amended

--- models/projects.js
+++ models/projects.js
@@ -4,16 +4,23 @@ let projectCollection = client.db("DB").collection("Tools");
 
 //Some Database Functions...
 //Insert some cards...
-const insertCard = (card, callback) =>{
-    projectCollection.insertOne(card, callback);
+const insertCard = (card, callback) => {
+    projectCollection.insertOne(card, (err) => {
+        if (err) {
+            console.log("Error Message: \n" + err);
+        } else {
+            console.log("Card successfully inserted!")
+        }
+    });
 }
 
 
 //Find some cards...
-const getCard = (callback) =>{
+const getCard = (callback) => {
     projectCollection.find({}).toArray(callback);
 }
 
 module.exports = {
-    insertCard, getCard
+    insertCard,
+    getCard
 }
